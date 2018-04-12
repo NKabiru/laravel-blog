6a474de22ea87ee0553c4d6e2e42f134cc24aede
Njenga Kabiru <njengakabiru@gmail.com>
2018-04-12 12:32:05 +0300

    Add tasks view

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,13 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Tasks</title>
 </head>
 <body>
-    <ul>
-        @foreach($tasks as $task)
-            <li>{{ $task }}</li>
-        @endforeach
-    </ul>
+    <h1>{{ $task->body }}</h1>
 </body>
 </html>

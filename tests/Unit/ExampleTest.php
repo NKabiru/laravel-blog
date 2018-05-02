7af82b5ec96aada5c3822bb78e6377c3ffa6a61d
NKabiru <njengakabiru@gmail.com>
2018-05-02 07:49:20 +0000

    Add ep.22 testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Unit;
 
+use App\Post;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -14,6 +17,28 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+//        Given two instances of posts
+        $first = factory(Post::class)->create();
+
+        $second = factory(Post::class)->create([
+            'created_at' => Carbon::now()->subMonth()
+        ]);
+//        Apply the archives method
+        $posts = Post::archives();
+
+//        Assert that the method returns arrays with year,month,published
+        $this->assertEquals([
+            [
+                'year' => $first->created_at->format('Y'),
+                'month' => $first->created_at->format('F'),
+                'published' => 1
+            ],
+            [
+                'year' => $second->created_at->format('Y'),
+                'month' => $second->created_at->format('F'),
+                'published' => 1
+            ],
+        ], $posts);
+
     }
 }
